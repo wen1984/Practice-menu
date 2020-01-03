229d52b84025656bec9c38c15608d02ae5b61c65
wen chi yu <kyotofu28@gmail.com>
2020-01-03 21:58:38 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".menu").on("click", function() {
+        $(".menubox").show();
+    })
+
+    $("#closebtn").on("click", function() {
+        $(".menubox").hide();
+    })
+})
